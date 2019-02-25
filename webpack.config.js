80451240c18a042fc50b1de115ca313ca8afb306
YuanXuDong <yxd9187@163.com>
2019-02-25 21:31:51 +0800

    004.webpack-dev-server 的配置及使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  target: 'web',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'bundle.js',
@@ -41,5 +46,31 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLPlugin()
+  ]
 }
+
+if(isDev){
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    port: '8099',
+    host: '0.0.0.0',
+    overlay: {
+      errors: true,
+    },
+    hot: true,
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NodeEnvironmentPlugin()
+  )
+}
+
+module.exports = config
